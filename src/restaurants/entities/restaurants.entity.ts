29f6eb84a7dee1857aa11e8c63fbf98d31b638cc
coseo12 <momo_12@naver.com>
2020-12-07 19:15:29 +0900

    add Typeorm and nest

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,13 +1,30 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, InputType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 
+@InputType({ isAbstract: true })
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String)
+  @Column()
+  @IsString()
+  @Length(5)
   name: string;
-  @Field(type => Boolean, { nullable: true })
-  isVegan: true;
-  @Field(type => String)
+
+  @Field(type => Boolean, { defaultValue: false })
+  @Column({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field(type => String, { defaultValue: '강남' })
+  @Column({ default: '강남' })
+  @IsOptional()
+  @IsString()
   address: string;
-  @Field(type => String)
-  ownersName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,18 +1,41 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly rastaurantService: RestaurantService) {}
+
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.rastaurantService.getAll();
   }
+
   @Mutation(returns => Boolean)
-  createRestaurant(
-    @Args()
-    createRestaurantDto: CreateRestaurantDto,
-  ): boolean {
-    return true;
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.rastaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(
+    @Args('update') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    try {
+      await this.rastaurantService.updateRestaurant(updateRestaurantDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
